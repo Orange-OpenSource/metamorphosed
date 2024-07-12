3555805d39e2afaf838641e849b48fedcac3377a
Johannes Heinecke <johannes.heinecke@orange.com>
2024-07-12 13:04:02 +0200

    added

---
+++ edge_predictor.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+# This library is under the 3-Clause BSD License
+#
+# Copyright (c) 2022-2024,  Orange
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of Orange nor the
+#      names of its contributors may be used to endorse or promote products
+#      derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL ORANGE BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# Software Name: MetAMoRphosED AMR-Editor
+# Author: Johannes Heinecke
+
+# Predicts edges between two instances or an instance and a literal
+# subclass this class to add your own predictor
+
+import re
+
+
+class Basic_EdgePredictor:
+    def __init__(self, **kwargs):
+        self.verbre = re.compile(r"-\d{2,3}$")
+
+    def predict(self, source_concept, target_concept):
+        # very basic implementation, use some classifier trained on your data to improve
+        if target_concept == "name":
+            return ":name"
+        if source_concept in ["name", "and", "or"]:
+            return ":op2"
+
+        if self.verbre.search(source_concept):
+            return ":ARGn"
+        # not able to predict
+        return "todo"
